ec89300a8c2fa49f84429c2073170cd1d11e961c
fergaschas <ferrangascongalindo@gmail.com>
2023-06-21 22:47:49 +0200

    add auth service and provider

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,15 @@
+import 'package:learningdart/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  });
+  Future<void> logout();
+  Future<void> sendEmailVerification();
+}
---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,43 @@
+import 'auth_provider.dart';
+import 'auth_user.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider provider;
+
+  AuthService(this.provider);
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) {
+    return provider.createUser(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  }) {
+    return provider.login(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> logout() {
+    return provider.logout();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    return provider.sendEmailVerification();
+  }
+}
